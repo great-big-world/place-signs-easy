39b817b5129cb5d7224a29ccf5cf00f1bbdbbe8a
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2026-04-01 21:57:16 -0400

    Initial commit

---
+++ src/main/java/dev/creoii/placesignseasy/PlaceSignsEasy.java
@@ -0,0 +1,10 @@
+package dev.creoii.placesignseasy;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PlaceSignsEasy implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/dev/creoii/placesignseasy/client/PlaceSignsEasyClient.java
@@ -0,0 +1,10 @@
+package dev.creoii.placesignseasy.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PlaceSignsEasyClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
